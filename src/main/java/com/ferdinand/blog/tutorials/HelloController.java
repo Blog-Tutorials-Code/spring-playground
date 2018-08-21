7157a937b7e14dd58684f16f214f381b0e0f9465
Ferdinand Agyei-Yeboah <Ferdinandyeboah@gmail.com>
2018-08-20 21:50:45 -0400

    Intial scaffold of playground

---
+++ src/main/java/com/ferdinand/blog/tutorials/HelloController.java
@@ -0,0 +1,14 @@
+package com.ferdinand.blog.tutorials;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "test/users", method = RequestMethod.GET)
+    public String getUsers(){
+        return "Returning List of Users";
+    }
+
+}
---
+++ src/main/java/com/ferdinand/blog/tutorials/SpringConfig.java
@@ -0,0 +1,8 @@
+package com.ferdinand.blog.tutorials;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+}
